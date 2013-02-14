29bfd2daabe31cf7db10b021137fd29c859945a9
brotatos <xrobinc@gmail.com>
2013-02-13 23:04:12 -0800

    modified:   scratch.py

--- scratch.py
+++ scratch.py
@@ -21,3 +21,31 @@ command()
 
 # do 'actions = [jump, attack]' and then 'action[0]()'
 # think of '()' as the "call" operator
+
+# class practice
+
+class blankCharacter:
+    HP = 0
+    MP = 0
+
+class Mage:
+    HP = 100
+    MP = 500
+    def description():
+        return "You are now an amazing wizard."
+    def g():
+        return 'hello world'
+    h = g()
+    d = description()
+
+print blankCharacter.HP + Mage.HP
+
+character = Mage()
+
+print character.MP 
+print Mage.h
+print character.h
+
+blankCharacter = Mage
+print blankCharacter.HP
+print Mage.d
