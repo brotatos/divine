13c29a5b648fbe0b62002ec3d5acdb0963dd8951
Robin Choudhury <xrobinc@gmail.com>
2013-06-25 18:17:36 -0700

    Switch to cartesian coordinates for rooms.

--- divine.py
+++ divine.py
@@ -3,114 +3,120 @@
 #       DIVINE        #     W   E
 #                     #       S
 # u_l     u_m     u_r #
-# 1##     2##     3## #
+# A##     B##     C## #
 # ###     ###     ### #
 # ###     ###     ### #
 #                     #
 # m_l     m_m     m_r #
-# 4##     5##     6## #
+# D##     E##     F## #
 # ###     ###     ### #
 # ###     ###     ### #
 #                     #
 # b_l     b_m     b_r #
-# 7##     8##     9## #
+# G##     H##     I## #
 # ###     ###     ### #
 # ###     ###     ### #
 #                     #
 #        enter        #
 #######################
 
-import random, sys
+import random, sys, collections
 
-TREASURE = random.randrange(1, 9)
-
-print """You awake as a thief in search of gold in front of Divine, a mystical castle.
-You believe the treasure is here; however, you may choose to enter or leave.
-"""
+TREASURE_X = random.randrange(0, 2)
+TREASURE_Y = random.randrange(0, 2)
+X_MIN = 0
+X_MAX = 2
+Y_MIN = 0
+Y_MAX = 2
 
 class Room:
-    def __init__(self, location):
-        if location == "upperLeft":
-            self.number = 1
-            self.dirs = {"east": "upperMid", "south": "midLeft"}
-        elif location == "upperMid":
-            self.number = 2
-            self.dirs = {"east": "midMid", "south": "botMid", "west": "upperLeft"}
-        elif location == "upperRight":
-            self.number = 3
-            self.dirs = {"south": "midRight"  , "west": "midMid"}
-        elif location == "midLeft":
-            self.number = 4
-            self.dirs = {"north": "upperLeft", "east": "midMid", "south": "botLeft"}
-        elif location == "midMid":
-            self.number = 5
-            self.dirs = {"north": "upperMid", "east": "midLeft", "south": "botMid", "west": "midLeft"}
-        elif location == "midRight":
-            self.number = 6
-            self.dirs = {"south", "west", "east"}
-        elif location == "botLeft":
-            self.number = 7
-            self.dirs = {"north", "east"}
-        elif location == "botMid":
-            self.number = 8
-            self.dirs = {"north": "midMid", "east": "botRight", "west": "botLeft"}
-        elif location == "botRight":
-            self.number = 9
-            self.dirs = {"north": "midRight", "east": "botMid"}
+    def __init__(self, x, y):
+        if x < X_MIN or x > X_MAX:
+            raise Exception("Invalid x coordinate.")
         else:
-            raise Exception("Invalid room location.")
+            self.x = x
 
+        if y < Y_MIN or y > Y_MAX:
+            raise Exception("Invalid y coordinate.")
+        else:
+            self.y = y
 
     def getDirections(self):
         """ Prints all possible directions to move in. """
+        dirs = []
 
-        for directions in self.dirs:
-            print "\t" + directions
+        if self.x == X_MIN:
+            dirs.append("east")
+        elif self.x == X_MIN + 1:
+            dirs.append("east")
+            dirs.append("west")
+        else:
+            dirs.append("west")
 
+        if self.y == Y_MIN:
+            dirs.append("north")
+        elif self.y == Y_MIN + 1:
+            dirs.append("north")
+            dirs.append("south")
+        else:
+            dirs.append("south")
+
+        return dirs
 
 class Occupant:
     def __init__(self):
-        self.location = Room("botMid")
+        self.location = Room(random.randrange(0, 2), random.randrange(0, 2))
 
     def getLocation(self):
-        chambers = {
-                    1: "Upper Left Chamber",
-                    2: "Upper Middle Chamber",
-                    3: "Upper Right Chamber",
-                    4: "Middle Left Chamber",
-                    5: "Middle Middle Chamber",
-                    6: "Middle Right Chamber",
-                    7: "Bottom Left Chamber",
-                    8: "Bottom Middle Chamber",
-                    9: "Bottom Right Chamber",
-        }
-        return chambers[self.location.number]
-
+        names = [ ["The Solar", "The Mezzanine", "The Lords & Ladies Chamber"],
+                  ["The Bower", "The Great Hall", "The Bottlery"],
+                  ["The Chapel", "The Oratory", "The Bailey"],
+                ]
+        return names[self.location.x][self.location.y]
 
 def move(Occupant):
     """ Move occupant based off direction.
     Should also print the current room location.
     """
 
-    print "You are currently in: " + self.getLocation()
+    print "You are currently at: " + Occupant.getLocation()
     print "Which direction would you like to go?"
+    dirs = Occupant.location.getDirections()
+    for i in dirs:
+        print "\t" + i
 
-    dirs = set(self.location.dirs)
-    self.location.getDirections()
+    checkdirs = set(dirs)
     direction = raw_input("> ")
 
-    if direction in dirs:
-        # self.location = roomVariable
-        self.location = Room(self.location.dirs[direction])
+    if direction in checkdirs:
+        if direction == "east":
+            Occupant.location.x += 1
+        elif direction == "west":
+            Occupant.location.x -= 1
+        elif direction == "north":
+            Occupant.location.y += 1
+        else:
+            Occupant.location.y -= 1
+        print "You are now here: " + Occupant.getLocation()
     else:
-        self.move()
+        move(Occupant)
 
-def getStatus(Occupant):
+def checkTreasure(Occupant):
     """ Determines if you have won the game or not. """
-    # if occupant.location.number == TREASURE: win()
-    if self.location.number == TREASURE:
+    if Occupant.location.x == TREASURE_X and Occupant.location.y == TREASURE_Y:
         win()
 
-def win():
-    print "You have found the treasure!"
+def win(Occupant):
+    print "You have found the treasure in %s!" % Occupant.getLocation()
     sys.exit()
+
+if __name__ == '__main__':
+    robin = Occupant()
+    move(robin)
+    room = Room(0, 1)
+    room.getDirections()
+    #print """You awake as a thief in search of gold in front of Divine, a mystical castle.
+    #You believe the treasure is here; however, you may choose to enter or leave.
+    #"""
+    #robin = Occupant()
+    #move(robin)
